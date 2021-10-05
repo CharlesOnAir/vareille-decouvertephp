fe69b441bc8f151830245d7cb7d784d53c522913
Charles Sauvat <charles.sauvat@hotmail.com>
2021-10-05 11:47:52 +0200

    ranger.php

--- ranger.php
+++ ranger.php
@@ -6,9 +6,8 @@ array_shift($arguments);
 // Je crée un tableau
 $datas = array();
 // Je boucle sur le tableau
-foreach($arguments as $argument) {
+foreach($arguments as $argument)
     // Je push mes données dans mon tableau
     array_push($datas, $argument);
-}
 // Je retourne le json_encode du tableau
 echo json_encode($datas);
